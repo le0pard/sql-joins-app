54a77c2457d8a62bdae1d92d612b0d65793f310b
le0pard <le0pard@users.noreply.github.com>
2022-02-14 09:19:53 +0000

    Deploying to gh-pages from @ le0pard/sql-joins-app@55fd994a17722511107ddfa2704b556b8898809f 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const timestamp = 1644829699148;
+const timestamp = 1644830391982;
 const build = [
-  "/_app/start-d59f415a.js",
+  "/_app/start-4667e7d9.js",
   "/_app/pages/__layout.svelte-308dfc07.js",
   "/_app/assets/pages/__layout.svelte-5403f75c.css",
   "/_app/error.svelte-94fb5597.js",
-  "/_app/pages/index.svelte-28f68a29.js",
-  "/_app/assets/pages/index.svelte-d1184b44.css",
+  "/_app/pages/index.svelte-2587ff82.js",
+  "/_app/assets/pages/index.svelte-4c0fa22a.css",
   "/_app/chunks/vendor-88af7281.js"
 ];
 const files = [
