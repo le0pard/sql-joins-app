827704efe9c18992e022d4dd5b5f58ffcf43d43c
le0pard <le0pard@users.noreply.github.com>
2022-06-12 13:54:15 +0000

    Deploying to gh-pages from @ le0pard/sql-joins-app@0f45e194ec37305f51394059a99ea09c08a1b77c 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 const build = [
-  "/_app/start-71f2d77c.js",
-  "/_app/pages/__layout.svelte-2c1b7288.js",
-  "/_app/assets/pages/__layout.svelte-29c2b141.css",
-  "/_app/error.svelte-ebd0a545.js",
-  "/_app/pages/index.svelte-812ff5fe.js",
-  "/_app/assets/pages/index.svelte-9152034b.css",
-  "/_app/chunks/index-349d637e.js"
+  "/_app/immutable/start-293d631c.js",
+  "/_app/immutable/pages/__layout.svelte-972addcd.js",
+  "/_app/immutable/assets/pages/__layout.svelte-fcfd45e0.css",
+  "/_app/immutable/error.svelte-ebd0a545.js",
+  "/_app/immutable/pages/index.svelte-592e2a7c.js",
+  "/_app/immutable/assets/pages/index.svelte-4c0fa22a.css",
+  "/_app/immutable/chunks/index-349d637e.js"
 ];
 const files = [
   "/apple-touch-icon.png",
@@ -16,7 +16,7 @@ const files = [
   "/manifest.webmanifest",
   "/maskable_icon.png"
 ];
-const version = "1652998015351";
+const version = "1655042051220";
 const CACHE_NAME = `sql-joins-${version}`;
 var activateEvent = (event) => {
   event.waitUntil(caches.keys().then((keys) => keys.filter((key) => key !== CACHE_NAME)).then((keysToRemove) => Promise.all(keysToRemove.map((key) => caches.delete(key)))).then(() => self.clients.claim()));
