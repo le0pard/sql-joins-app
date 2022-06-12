51f4cb037d240dda0671b4003668ec4c60c58c5c
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-06-12 16:52:54 +0300

    up

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,6 @@ import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  compilerOptions: {
-    cssHash: ({hash, css}) => `sj-${hash(css)}`
-  },
   kit: {
     adapter: adapter(),
     serviceWorker: {
