2e6f2b6f1c8e28b6d6fa2fc1a4a245d2ebc76f89
le0pard <le0pard@users.noreply.github.com>
2022-07-18 15:43:07 +0000

    Deploying to gh-pages from @ le0pard/sql-joins-app@aba32ca07ff6e6a4d87f5c9caf547bdd089153cf 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const n = [
-  "/_app/immutable/start-8bcaf298.js",
+  "/_app/immutable/start-07f109c1.js",
   "/_app/immutable/pages/__layout.svelte-420e7561.js",
   "/_app/immutable/assets/__layout-30d161eb.css",
   "/_app/immutable/error.svelte-18aa0353.js",
@@ -14,7 +14,7 @@ const n = [
   "/icon-512.png",
   "/manifest.webmanifest",
   "/maskable_icon.png"
-], l = "1658158876913", a = `sql-joins-${l}`, c = (t) => {
+], l = "1658158983546", a = `sql-joins-${l}`, c = (t) => {
   t.waitUntil(caches.keys().then((e) => e.filter((s) => s !== a)).then((e) => Promise.all(e.map((s) => caches.delete(s)))).then(() => self.clients.claim()));
 }, p = (t) => {
   t.waitUntil(caches.open(a).then((e) => Promise.all([
