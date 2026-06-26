5f4e69ced16bb684f7cfa5fb0def24dac716c098
Oleksii Vasyliev <leopard.not.a@gmail.com>
2026-06-26 18:20:29 +0300

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import path from 'path'
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 import {sveltekit} from '@sveltejs/kit/vite'
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [
     sveltekit({
+      adapter: adapter(),
+      serviceWorker: {
+        register: true,
+        files: (filepath) => /\.(png|svg|ico|webmanifest)$/.test(filepath)
+      },
       compilerOptions: {
         cssHash: ({ hash, css }) => `s-${hash(css)}`
-      }
+      },
+      preprocess: [
+        preprocess({
+          postcss: true
+        })
+      ]
     })
   ],
   resolve: {
